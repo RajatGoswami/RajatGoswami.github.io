c9e9d6b4de3be3bd2e181d0f90e30baa54398d1b
Rajat Goswami <rajat.goswami@hotmail.com>
2018-01-07 18:10:33 -0500

    Load gif depending on time of day

---
+++ js/change-gif.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    let currentDate = new Date();
+	let hours = currentDate.getHours();
+	if(hours > 22 || hours < 8) { 
+		document.getElementById("bear-gif").src = "images/Snooze.gif";
+	}
+});
