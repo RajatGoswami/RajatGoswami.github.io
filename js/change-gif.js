0e225a7b5ec5f8e3a138aa6934d8d67ca6f620d3
Rajat Goswami <rajat.goswami@hotmail.com>
2018-01-07 22:27:22 -0500

    Changing time constraint

--- js/change-gif.js
+++ js/change-gif.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     let currentDate = new Date();
 	let hours = currentDate.getHours();
-	if(hours > 22 || hours < 8) { 
+	if(hours > 21 || hours < 8) {
 		document.getElementById("bear-gif").src = "images/Snooze.gif";
 	}
 });
