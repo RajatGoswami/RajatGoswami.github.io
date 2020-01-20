af0b52bfceebeaa00122c026eeda22f3d92cf101
Rajat Goswami <rajat.goswami@hotmail.com>
2020-01-20 01:01:09 -0500

    Website makeover

--- js/change-gif.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    let currentDate = new Date();
-	let hours = currentDate.getHours();
-	if(hours > 21 || hours < 8) {
-		document.getElementById("bear-gif").src = "images/Snooze.gif";
-	}
-});
--- js/scrolling.js
+++ js/scrolling.js
@@ -1,3 +1,14 @@
-function scrollToNextSection() {
-	$("html, body").animate({scrollTop: $("#contact-me").offset().top}, "slow");
-}
+$(document).ready(function(){
+	$("a").on('click', function(e) {
+
+		if (this.hash !== "") {
+			e.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $(this.hash).offset().top
+			}, 800, function(){
+				window.location.hash = this.hash;
+			});
+		}
+	});
+});
